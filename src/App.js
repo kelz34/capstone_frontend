73f7403f263cee03f35d9fb0681cc802ee24e8ee
Kelvin <kelzlaur34@gmail.com>
2023-12-07 15:14:01 -0500

    first commit front

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import './App.css';
+
+// components
+
+import InputElements from "./components/InputElements"
+
+function App() {
+  return (
+   <Fragment>
+    <div className="container">
+      <InputElements />
+    </div>
+    
+   </Fragment>
+  );
+}
+
+export default App;

51904001d4558ed1d9de4cfc54bd3d3e06516490
Kelvin <kelzlaur34@gmail.com>
2023-12-12 14:56:03 -0500

    footer updated

--- src/components/HomePageIcons.js
+++ src/components/HomePageIcons.js
@@ -2,19 +2,33 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const HomePageIcons = () => {
-    return <nav className="nav">
-        <Link to="/" className="site-title">Chem Cheats</Link>
+    return (
+    <nav className="nav">
+        <h1 class="text-center text-6xl">Study Tools</h1>
         <ul>
-            <li>
-                <h3>Flash Cards</h3>
-                <Link to="/topics"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVuJ-d_F6BNt3LHr0YEGPaYqsJ0DxNAOdpKg&usqp=CAU"/></Link>
-            </li>
-            <li>
-                <h3>Lessons</h3>
-                <Link to="/lessons"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3cZ0Q2rJm4OOcQmPFpkj3JYAh46XiIHjmsQ&usqp=CAU"/></Link>
-            </li>
-        </ul>
+            <div class="flex items-start place-content-around">
+                <div class="px-10">
+                    <li class="row-span-3 ...">
+                        <h3 class="text-center text-3xl">Flash Cards</h3>
+                        <Link to="/topics"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVuJ-d_F6BNt3LHr0YEGPaYqsJ0DxNAOdpKg&usqp=CAU"/></Link>
+                    </li>
+                </div>
+                <div class="px-10">
+                    <li>
+                        <h3 class="text-center text-3xl">Lessons</h3>
+                        <Link to="/lessons"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3cZ0Q2rJm4OOcQmPFpkj3JYAh46XiIHjmsQ&usqp=CAU"/></Link>
+                    </li>
+                </div>
+            </div>
+        </ul>        
     </nav>
-}
+)}
 
 export default HomePageIcons; 
+
+
+// {/* <div class="flex items-start ...">
+//   <div >01</div>
+//   <div >02</div>
+//   <div >03</div>
+// </div> */}
